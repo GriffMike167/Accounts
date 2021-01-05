c3f649ff5601f9ece12eccab0554601d9870c829
Michael Griffith <griffithcmichael@gmail.com>
2021-01-05 00:03:12 -0600

    pulled the and pathed the files correctly

--- server.js
+++ server.js
@@ -15,14 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/accounts', {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("../../../../Accounts/routes/api.js.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+// mongodb+srv://griffmike167:Irish4528@cluster0.pp5ya.mongodb.net/accounts?retryWrites=true&w=majority

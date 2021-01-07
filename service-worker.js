d434a636e04e88adece7abf16f3fe5ab0e9bad64
Michael Griffith <griffithcmichael@gmail.com>
2021-01-06 20:03:46 -0600

    changed file location and fixed both index file

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,12 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  "/public/index.html",
-  "/favicon.ico",
-  "/manifest.webmanifest",
-  "/public/style.css",
-  "/public/index.js",
-  "/routes/api.js",
-  "/public/icons/icon-192x192.png",
-  "/public/icons/icon-512x512.png"
+  "/index.html",
+  "/assets/css/style.css",
+  "/assets/js/index.js",
+  "/assets/js/db.js",
+  "/assets/images/icon/icon-192x192.png",
+  "/assets/images/icon/icon-512x512.png"
 
 ];
 
